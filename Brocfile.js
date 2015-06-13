47302f1f57aea122ee0f8c1e1c8c657e6ecd26bf
Jeff-Coons <Jeffcoons@live.com>
2015-06-13 16:07:32 -0400

    initial commit

--- Brocfile.js
+++ Brocfile.js
@@ -2,7 +2,24 @@
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-var app = new EmberApp();
+var app = new EmberApp({
+
+    minifyCSS: {
+        enabled: false
+    },
+
+    minifyJS: {
+        enabled: false
+    },
+
+    sourcemaps: {
+        enabled: true,
+        extensions: ['css', 'js']
+    },
+
+
+
+});
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
@@ -16,5 +33,8 @@ var app = new EmberApp();
 // modules that you would like to import into your application
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
+
+app.import('bower_components/bourbon/app/assets/stylesheets/bourbon.scss');
+app.import('bower_components/bourbon/app/assets/stylesheets/neat.scss');
 
 module.exports = app.toTree();
